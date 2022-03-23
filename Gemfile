8bc4fd70187d30e5934af1acb912d6f180191db0
Santiago Velosa <santiagovelosa@gmail.com>
2022-03-23 15:20:45 -0500

    Add posts_request_spec.rb

--- Gemfile
+++ Gemfile
@@ -53,6 +53,7 @@ group :development, :test do
   gem 'debug', platforms: %i[mri mingw x64_mingw]
   # Rspec installation
   gem 'rspec-rails', '~> 5.0.0'
+  gem 'rails-controller-testing'
 end
 
 group :development do
---
+++ spec/requests/posts_request_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+require 'rails-controller-testing'
+
+RSpec.describe 'Posts', type: :request do
+  describe 'GET #index' do
+    before(:example) { get '/users/:user_id/posts' }
+
+    it 'is a success' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "renders 'index' template" do
+      expect(response).to render_template('index')
+    end
+
+    it 'response body includes correct placeholder text' do
+      expect(response.body).to include("This should be a page where you can see comments for the user's posts")
+    end
+  end
+
+  describe 'GET #show', type: :request do
+  before(:example) { get '/users/:user_id/posts/:id' }
+
+  it 'is a success' do
+    expect(response).to have_http_status(:ok)
+  end
+
+  it "renders 'index' template" do
+    expect(response).to render_template('show')
+  end
+
+  it 'response body includes correct placeholder text' do
+    expect(response.body).to include("This should be a page where you can see an individual post for an user")
+  end
+  end
+end
